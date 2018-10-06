97926e2fa62517c8b1d3082e6dccabeb7c4031c7
Robert Garner <rgarner011235@gmail.com>
2018-10-06 12:35:34 -0600

    Added Vacuum Manager

---
+++ Ten/Assets/Prefabs/FirstPersonControllerWithKirby/Scripts/VacuumTargetScript.cs
@@ -0,0 +1,17 @@
+﻿//VacuumTargetScript
+//Programmer: Rob Garner (rgarner011235@gmail.com)
+//Date: 6 Oct 2018
+//Purpose: Handle collisions when vacuum cleaner encounters objects.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VacuumTargetScript : MonoBehaviour {
+
+
+    //Method called when vacuum collides with target
+    public void VacuumedAction()
+    {
+        Debug.Log(this.name + " vacuumed");
+    }
+}
