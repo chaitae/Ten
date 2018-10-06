787b6c67c7a8bebacd6986eb9e04bc44b45b3b2c
thoa huynh <chaitaes@gmail.com>
2018-10-06 09:50:46 -0600

    Navmesh moves randomly

--- Ten/Assets/Ghost.cs
+++ Ten/Assets/Ghost.cs
@@ -5,22 +5,51 @@ using UnityEngine.AI;
 
 public class Ghost : MonoBehaviour {
     public Transform goal;
-    public float walkRadius = 2;
+    public float walkRadiusMax = 2;
+    public float walkRadiusMin = 4;
+    NavMeshAgent agent;
+    public float range = 10.0f;
 
     // Use this for initialization
-    void Start () {
-        Vector3 randomDirection = Random.insideUnitSphere * walkRadius;
+    void Start()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * walkRadiusMax;
 
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = goal.position;
-        randomDirection += transform.position;
-        NavMeshHit hit;
-        NavMesh.SamplePosition(randomDirection, out hit, walkRadius, 1);
-        Vector3 finalPosition = hit.position;
+        agent = GetComponent<NavMeshAgent>();
+        ChooseNewLocation();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void ChooseNewLocation()
+    {
+        Vector3 point;
+        while (!RandomPoint(transform.position,range,out point))
+        {
+
+        }
+        Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f);
+        agent.SetDestination(point);
+
+    }
+    bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Vector3 randomPoint = center + Random.insideUnitSphere * range;
+            NavMeshHit hit;
+            if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+            {
+                result = hit.position;
+                return true;
+            }
+        }
+        result = Vector3.zero;
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(!agent.hasPath)
+        {
+            ChooseNewLocation();
+        }
+    }
 }
