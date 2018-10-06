b639099bcd4239b3a2e2510619741078bb4550ee
thoa huynh <chaitaes@gmail.com>
2018-10-06 12:31:59 -0600

    Dialogue updated and added script and sprites folder

---
+++ Ten/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+[System.Serializable]
+public class Dialogue{
+    public string name;
+    [TextArea(0, 10)]
+    public string[] sentences;
+    public string optionText;
+    public GameObject activatableGO;
+    public Dialogue[] options;
+    public Dialogue(string[] tempSentences)
+    {
+        sentences = tempSentences;
+        options = new Dialogue[0];
+    }
+
+}
---
+++ Ten/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+public class DialogueTrigger : MonoBehaviour,IInteractable {
+    public Dialogue dialogue;
+    public delegate void endConversation();
+    public event endConversation OnEndConversationHandler;
+    public bool triggerOnAwake = false;
+    public AudioSource audioSource;
+    float currTime = 0;
+    public GameObject activatableGO;
+    bool calledDialogue = false;
+    bool isInDialogue = false;
+    void OnEnable()
+    {
+        DialogueManager.OnEndConversationHandler += SetDialogueOff;
+        DialogueManager.OnStartConversationHandler +=SetDialogueOn;
+    }
+    void SetDialogueOn()
+    {
+        isInDialogue = true;
+    }
+    void SetDialogueOff()
+    {
+        isInDialogue = false;
+    }
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            TriggerDialogue();
+        }
+        currTime += Time.deltaTime;
+        if(currTime >=.3f && triggerOnAwake && !calledDialogue)
+        {
+            calledDialogue = true;
+            TriggerDialogue();
+        }
+
+    }
+    public void interact()
+    {
+        if(!isInDialogue)
+        TriggerDialogue();
+    }
+
+    public void interact(KeyCode key)
+    {
+        audioSource = GetComponent<AudioSource>();
+        if(audioSource != null)
+        audioSource.Play();
+        if(key == KeyCode.E)
+        {
+            TriggerDialogue();
+
+        }
+    }
+    void UpdateDialogue()
+    {
+        dialogue = DialogueManager.instance.currDialogue;
+        Dialogue[] bah = new Dialogue[0];
+    }
+    public void EndConversationBehavior()
+    {
+        if (OnEndConversationHandler != null)
+            OnEndConversationHandler();
+        if (activatableGO != null)
+        {
+            IActivatable[] activatables = activatableGO.GetComponents<IActivatable>();
+            foreach(IActivatable activatable in activatables)
+            {
+                activatable.Activate();
+            }
+
+        }
+        DialogueManager.OnEndConversationHandler -= EndConversationBehavior;
+        DialogueManager.OnEndConversationHandler -= UpdateDialogue;
+    }
+    public void TriggerDialogue()
+    {
+        DialogueManager.instance.StartDialogue(dialogue);
+        //if(DoEndBehavior)
+        DialogueManager.OnEndConversationHandler += EndConversationBehavior;
+        DialogueManager.OnEndConversationHandler += UpdateDialogue;
+
+
+    }
+
+}
